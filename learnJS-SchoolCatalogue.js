65212fc5ca07966af53729f96624ecb2194bf8c1
Kathryn Jackson <65988644+kathrynljackson@users.noreply.github.com>
2021-04-06 18:00:18 -0400

    Complete

---
+++ learnJS-SchoolCatalogue.js
@@ -0,0 +1,78 @@
+//Goal: Let’s put your knowledge of classes to the test by creating a digital school catalog for the New York City Department of Education. The Department of Education wants the catalog to hold quick reference material for each school in the city.
+
+//We need to create classes for primary and high schools.
+
+class School {
+  constructor(name, level, numberOfStudents){
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name(){
+    return this._name
+  }
+
+  get level(){
+    return this._level
+  }
+
+  get numberOfStudents(){
+    return this._numberOfStudents
+  }
+
+  set numberOfStudents(newNumberOfStudents){
+    if(typeof newNumberOfStudents === 'number'){
+      this._numberOfStudents = newNumberOfStudents
+    } else {
+      console.log('Invalid input: numberOfStudents must be set to a Number.')
+    }
+  }
+
+  quickFacts(){
+    console.log(`${this._name} educates ${this._numberOfStudents} at the ${this._level} school level.`)
+  }
+
+  static pickSubstituteTeacher(list){
+    let randomNumber = Math.floor(Math.random() * list.length)
+    return list[randomNumber]
+  }
+
+}
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy){
+    super(name, 'primary', numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy(){
+    return this._pickupPolicy;
+  }
+}
+
+class MiddleSchool extends School {
+  constructor(name, numberOfStudents){
+    super(name, 'middle', numberOfStudents);
+  }
+}
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams){
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams(){
+    return this._sportsTeams
+  }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+
+const subs = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+console.log('Today\'s Sub is:', subs)
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+
+console.log(alSmith.sportsTeams)
